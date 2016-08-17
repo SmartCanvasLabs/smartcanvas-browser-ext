df21ee41b98f80e2680c155485076e892e244c41
Lucas <lucasp@ciandt.com>
2016-08-17 17:28:54 -0300

    adjusting build.

--- src/chrome/gulpfile.babel.js
+++ src/chrome/gulpfile.babel.js
@@ -38,8 +38,19 @@ gulp.task('lint', lint('app/scripts.babel/**/*.js', {
   }
 }));
 
-gulp.task('images', () => { 
+gulp.task('images', () => {
   return gulp.src('app/images/**/*')
+    .pipe($.if($.if.isFile, $.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    }))
+    .on('error', function (err) {
+      console.log(err);
+      this.end();
+    })))
     .pipe(gulp.dest(ENV + '/images'));
 });
 
